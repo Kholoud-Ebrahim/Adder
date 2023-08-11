011ec35bc177de425afd2b5f1b690452e07cab52
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-08-11 15:20:32 +0300

    half_adder

--- Half Adder/rtl/half_adder.v
+++ Half Adder/rtl/half_adder.v
@@ -1,1 +1,16 @@
+module half_adder (clk, rst, a, b, sum, carry);
+    input clk, rst;
+    input [2:0] a, b;
+    output reg [2:0] sum;
+    output reg carry;
 
+    always @(posedge clk) begin
+        if (rst) begin
+            sum   <= 0;
+            carry <= 0;
+        end
+        else begin
+            {carry, sum} <= a + b;
+        end
+    end
+endmodule
